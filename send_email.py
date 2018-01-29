a1b1b60c89f1224da7c43fd1bab2ac6ccf980d72
RAJ KUMAR <rahul2521994@gmail.com>
2018-01-29 11:24:46 +0530

    Add files via upload

---
+++ send_email.py
@@ -0,0 +1,49 @@
+##Send an html email using SMTP service
+
+from pyathenajdbc import connect
+from pyathenajdbc.util import as_pandas
+import pandas as pd
+from tabulate import tabulate
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import os
+from datetime import datetime,timedelta
+
+
+
+
+SMTP_host = 'email-smtp.us-west-2.amazonaws.com'
+SMTP_port = '587'
+SMTP_username = 'AKIAIQ6XDBEJ5SXK5ZIQ'
+SMTP_password = 'AkAGnvspLA05yNM0tLyC7ceLPe0IVWrouJaItKCybdZT'
+sender = 'automated-notifications@c1exchange.com'
+from_email = 'automated-notifications@c1exchange.com'
+
+
+emailBody = """
+<html><body><p>Revenue Numbers</p>
+{RequestsDaily}
+<p>Request Block numbers:</p>
+{RequestSummary}
+</body></html>
+"""
+
+html = emailBody.format(RequestsDaily = RequestsDaily.to_html(),
+                        RequestSummary = RequestSummary.to_html(index=False))
+
+message = MIMEMultipart(
+    "alternative", None, [MIMEText(html,'html')])
+
+message['Subject'] = 'XXXX Report'+' '+str(year)+'-'+str(month)+'-'+str(day)
+message['From'] = from_email
+message['To'] = 'raj.k.stats@gamil.com'
+server = smtplib.SMTP(SMTP_host)
+server.ehlo()
+server.starttls()
+server.login(SMTP_username, SMTP_password)
+server.sendmail(from_email, 'raj.k.stats@gmail.com', message.as_string())
+server.quit()
+
+
+
